cff6e2ed3269d12e9928e68a374bd90bd4980cb2
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-11-20 12:46:17 -0500

    added chat container

--- public/script.js
+++ public/script.js
@@ -1,9 +1,4 @@
-const message = document.getElementById("message")
-const handel = document.getElementById("handle")
-const sendbtn = document.getElementById("send")
-const output = document.getElementById("output")
-const isTyping = document.getElementById("isTyping")
-
+const canvas = document.getElementById('vid-canvas')
 
 // make connection 
 const socket = io()
@@ -12,38 +7,37 @@ socket.on('welcome', (data) => {
     console.log("You have connected to the server")
 })
 
-socket.on('chat', (data) => {
-    output.innerHTML += `<p><strong> ${data.handel}: </strong> ${data.message} </p>`
-    // if (isTypingNode.innerHTML === "") {
-    // }
-})
+
+
+
+
 
 // recieve typing detection
 socket.on('isTyping', function (data) {
-    console.log(`${data.handel} is typing`)
-    // if (isTyping.value ===""){
-    // }
+    // console.log(`${data.handel} is typing`)
+    // // if (isTyping.value ===""){
+    // // }
 
-    isTyping.innerHTML = `<em> ${data.handel} is typing </em>`
+    // isTyping.innerHTML = `<em> ${data.handel} is typing </em>`
 })
 
 
 // emit on typing detection
-message.addEventListener('input', () => {
-    console.log(`message input has changed: ${message.value}`)
+// message.addEventListener('input', () => {
+//     console.log(`message input has changed: ${message.value}`)
 
-    // While a user did not press send or is still working on a message
+//     // While a user did not press send or is still working on a message
 
-    socket.emit('isTyping', { handel: handel.value })
-})
+//     socket.emit('isTyping', { handel: handel.value })
+// })
 
-sendbtn.addEventListener('click', () => {
-    socket.emit('chat', {
-        message: message.value,
-        handel: handel.value
-    })
-    message.value = ""
-    isTyping.innerHTML = ""
-})
+// sendbtn.addEventListener('click', () => {
+//     socket.emit('chat', {
+//         message: message.value,
+//         handel: handel.value
+//     })
+//     message.value = ""
+//     isTyping.innerHTML = ""
+// })
 
 
