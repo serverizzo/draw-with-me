7ce4d1a054840dcfc26f620c512487c4492e9aee
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-11-20 16:17:01 -0500

    port is now set by local varriable (needed to deploy on heroku)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socket = require('socket.io')
 
 const app = express();
-const port = 3000
+port = process.env.PORT || 3000
 const server = app.listen(port, () => console.log(`running on ${port}`))
 
 app.use(express.static('public'))
